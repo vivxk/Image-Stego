452338ed6b6733acdb30b2cb2efdbc907c86a4e3
Vivek Sankhla <61795729+vivxk@users.noreply.github.com>
2021-11-09 08:36:31 +0530

    Add files via upload

---
+++ stego.py
@@ -0,0 +1,43 @@
+from PIL import Image
+ 
+# Convert  data into 8-bit binary form using ASCII 
+def genData(data):
+ 
+        # list of binary codes
+        # of given data
+        newd = []
+ 
+        for i in data:
+            newd.append(format(ord(i), '08b'))
+        return newd
+ 
+
+def modPix(pix, data):
+ 
+    datalist = genData(data)
+    lendata = len(datalist)
+    imdata = iter(pix)
+ 
+    for i in range(lendata):
+ 
+        # Extract 3 pixls at a time
+        pix = [value for value in imdata.__next__()[:3] +
+                                imdata.__next__()[:3] +
+                                imdata.__next__()[:3]]
+ 
+        # Pixel value odd-1 and even-0
+        for j in range(0, 8):
+            if (datalist[i][j] == '0' and pix[j]% 2 != 0):
+                pix[j] -= 1
+ 
+            elif (datalist[i][j] == '1' and pix[j] % 2 == 0):
+                if(pix[j] != 0):
+                    pix[j] -= 1
+                else:
+                    pix[j] += 1
+                # pix[j] -= 1
+ 
+        # Eighth pixel of every set tells
+        # whether to stop or read further.
+        # 0 means keep reading; 1 means thec
+        # message is over.  
